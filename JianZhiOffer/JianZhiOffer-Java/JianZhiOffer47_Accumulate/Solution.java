ecd1a4a5e95ddccd5776f7cd0b8cdf2c2a90fb0d
chenshuaiyu <664251867@qq.com>
2019-07-10 22:59:53 +0800

    JianZhiOffer47

--- JianZhiOffer/JianZhiOffer-Java/JianZhiOffer47_Accumulate/Solution.java
+++ JianZhiOffer/JianZhiOffer-Java/JianZhiOffer47_Accumulate/Solution.java
@@ -3,6 +3,7 @@ package JianZhiOffer47_Accumulate;
 public class Solution {
     public int Sum_Solution(int n) {
         int ans = n;
+        //利用短路作为递归返回入口
         boolean b = ((ans > 0) && ((ans += Sum_Solution(n - 1)) > 0));
         return ans;
     }
