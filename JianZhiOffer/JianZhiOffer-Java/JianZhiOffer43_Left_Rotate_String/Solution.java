acc81d7df5c728353e57737d1aa561d2396fedfb
chenshuaiyu <664251867@qq.com>
2019-07-11 07:16:54 +0800

    JianZhiOffer43

---
+++ JianZhiOffer/JianZhiOffer-Java/JianZhiOffer43_Left_Rotate_String/Solution.java
@@ -0,0 +1,21 @@
+package JianZhiOffer43_Left_Rotate_String;
+
+public class Solution {
+    public String LeftRotateString(String str, int n) {
+        if (str == null || str.length() == 0) return str;
+        int len = str.length();
+        n %= len;
+        StringBuilder ans = new StringBuilder();
+        ans.append(str, n, len);
+        ans.append(str, 0, n);
+        return ans.toString();
+    }
+
+    public String LeftRotateString1(String str, int n) {
+        if (str == null || str.length() == 0) return str;
+        int len = str.length();
+        n %= len;
+        str += str;
+        return str.substring(n, n + len);
+    }
+}
