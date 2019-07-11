a738eb354e1d4030992d9648fe21d4c36462022d
chenshuaiyu <664251867@qq.com>
2019-07-11 15:36:15 +0800

    JianZhiOffer65

---
+++ JianZhiOffer/JianZhiOffer-Java/JianZhiOffer65_Paths_In_The_Matrix/Solution.java
@@ -0,0 +1,31 @@
+package JianZhiOffer65_Paths_In_The_Matrix;
+
+public class Solution {
+    private int[] visited;
+    private boolean ans = false;
+
+    public boolean hasPath(char[] matrix, int rows, int cols, char[] str) {
+        visited = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i] == str[0] && !ans) {
+                helper(i, matrix, rows, cols, str, 0);
+            }
+        }
+        return ans;
+    }
+
+    private void helper(int i, char[] matrix, int rows, int cols, char[] str, int index) {
+        if (i < 0 || i >= matrix.length || visited[i] == 1 || matrix[i] != str[index] || ans)
+            return;
+        if (index == str.length - 1) {
+            ans = true;
+            return;
+        }
+        visited[i] = 1;
+        helper(i - 1, matrix, rows, cols, str, index + 1);
+        helper(i + 1, matrix, rows, cols, str, index + 1);
+        helper(i - cols, matrix, rows, cols, str, index + 1);
+        helper(i + cols, matrix, rows, cols, str, index + 1);
+        visited[i] = 0;
+    }
+}
