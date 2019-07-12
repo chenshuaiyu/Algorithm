ba3e1b2593dccf8631a53172b97caef78c06177b
chenshuaiyu <664251867@qq.com>
2019-07-12 09:30:11 +0800

    JianZhiOffer41

---
+++ JianZhiOffer/JianZhiOffer-Java/JianZhiOffer41_Continues_Sequence_with_Sum/Solution.java
@@ -0,0 +1,25 @@
+package JianZhiOffer41_Continues_Sequence_with_Sum;
+
+import java.util.ArrayList;
+
+public class Solution {
+    public ArrayList<ArrayList<Integer>> FindContinuousSequence(int sum) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        int l = 1, r = 2;
+        while (l < r) {
+            int n = (l + r) * (r - l + 1) / 2;
+            if (n == sum) {
+                ArrayList<Integer> list = new ArrayList<>();
+                for (int i = l; i <= r; i++)
+                    list.add(i);
+                ans.add(list);
+                l++;
+            } else if (n < sum) {
+                r++;
+            } else {
+                l++;
+            }
+        }
+        return ans;
+    }
+}
