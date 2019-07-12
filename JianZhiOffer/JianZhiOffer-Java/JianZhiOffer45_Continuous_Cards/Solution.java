49d25114cebecefa789dc3d99d6939668ab59cc7
chenshuaiyu <664251867@qq.com>
2019-07-13 07:33:20 +0800

    JianZhiOffer45

---
+++ JianZhiOffer/JianZhiOffer-Java/JianZhiOffer45_Continuous_Cards/Solution.java
@@ -0,0 +1,23 @@
+package JianZhiOffer.JianZhiOffer_Java.JianZhiOffer45_Continuous_Cards;
+
+import java.util.Arrays;
+
+public class Solution {
+    public boolean isContinuous(int[] numbers) {
+        if (numbers == null || numbers.length != 5) return false;
+        int len = numbers.length;
+        int zero = 0;
+        int need = 0;
+        Arrays.sort(numbers);
+        for (int i = 0; i < len; i++) {
+            if (numbers[i] == 0)
+                zero++;
+            else if (i + 1 < len) {
+                if (numbers[i] == numbers[i + 1])
+                    return false;
+                need += numbers[i + 1] - numbers[i] - 1;
+            }
+        }
+        return zero >= need;
+    }
+}
