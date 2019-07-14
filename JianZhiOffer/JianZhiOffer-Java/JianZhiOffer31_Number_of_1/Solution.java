aeb793a93a3a673c8cf54f2f4036f2cccd3756cc
chenshuaiyu <664251867@qq.com>
2019-07-14 13:32:32 +0800

    JianZhiOffer31

---
+++ JianZhiOffer/JianZhiOffer-Java/JianZhiOffer31_Number_of_1/Solution.java
@@ -0,0 +1,13 @@
+package JianZhiOffer.JianZhiOffer_Java.JianZhiOffer31_Number_of_1;
+
+public class Solution {
+    public int NumberOf1Between1AndN_Solution(int n) {
+        int ans = 0;
+        for (int i = 1; i <= n; i *= 10) {
+            int a = n / i;
+            int b = n % i;
+            ans += (a + 8) / 10 * i + ((a % 10 == 1) ? 1 : 0) * (b + 1);
+        }
+        return ans;
+    }
+}
