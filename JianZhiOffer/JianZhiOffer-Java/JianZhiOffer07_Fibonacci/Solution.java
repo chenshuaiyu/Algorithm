9a340a091020daadb4c13b6fa7bb0cad3d2df7a4
chenshuaiyu <664251867@qq.com>
2019-08-28 11:28:11 +0800

    review JianZhiOffer07

--- JianZhiOffer/JianZhiOffer-Java/JianZhiOffer07_Fibonacci/Solution.java
+++ JianZhiOffer/JianZhiOffer-Java/JianZhiOffer07_Fibonacci/Solution.java
@@ -1,8 +1,8 @@
-package JianZhiOffer07_Fibonacci;
+package JianZhiOffer.JianZhiOffer_Java.JianZhiOffer07_Fibonacci;
 
 public class Solution {
     public int Fibonacci(int n) {
-        if (n <= 2) return 1;
+        if (n <= 1) return n;
         int ans = 0, n1 = 0, n2 = 1;
         for (int i = 2; i <= n; i++) {
             ans = n1 + n2;
