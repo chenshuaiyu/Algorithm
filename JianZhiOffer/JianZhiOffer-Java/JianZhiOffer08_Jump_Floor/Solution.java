5cbcc3ed7c741d359293ecf7981787b949a6c7dd
chenshuaiyu <664251867@qq.com>
2019-08-28 11:34:45 +0800

    review JianZhiOffer08

--- JianZhiOffer/JianZhiOffer-Java/JianZhiOffer08_Jump_Floor/Solution.java
+++ JianZhiOffer/JianZhiOffer-Java/JianZhiOffer08_Jump_Floor/Solution.java
@@ -1,8 +1,11 @@
-package JianZhiOffer08_Jump_Floor;
+package JianZhiOffer.JianZhiOffer_Java.JianZhiOffer08_Jump_Floor;
 
 public class Solution {
+    //斐波那契数列
+
     public int JumpFloor(int target) {
         if (target <= 2) return target;
+        //可保存之前的状态
         int[] dp = new int[target + 1];
         dp[1] = 1;
         dp[2] = 2;
@@ -10,5 +13,16 @@ public class Solution {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[target];
+    }
+
+    public int JumpFloor1(int target) {
+        if (target <= 2) return target;
+        int ans = 0, n1 = 1, n2 = 2;
+        for (int i = 3; i <= target; i++) {
+            ans = n1 + n2;
+            n1 = n2;
+            n2 = ans;
+        }
+        return ans;
     }
 }
