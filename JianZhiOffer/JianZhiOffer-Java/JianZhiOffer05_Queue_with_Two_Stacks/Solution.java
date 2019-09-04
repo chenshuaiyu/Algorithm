56787a9085ff5509c8abac485c64dfb8296d5c57
chenshuaiyu <664251867@qq.com>
2019-09-04 19:10:25 +0800

    review JianZhiOffer05

--- JianZhiOffer/JianZhiOffer-Java/JianZhiOffer05_Queue_with_Two_Stacks/Solution.java
+++ JianZhiOffer/JianZhiOffer-Java/JianZhiOffer05_Queue_with_Two_Stacks/Solution.java
@@ -1,4 +1,4 @@
-package JianZhiOffer05_Queue_with_Two_Stacks;
+package JianZhiOffer.JianZhiOffer_Java.JianZhiOffer05_Queue_with_Two_Stacks;
 
 import java.util.Stack;
 
@@ -6,10 +6,12 @@ public class Solution {
     Stack<Integer> stack1 = new Stack<>();
     Stack<Integer> stack2 = new Stack<>();
 
+    //进队节点全部压入stack1
     public void push(int node) {
         stack1.push(node);
     }
 
+    //将stack1中的节点转移至stack2，取出stack2栈顶的值，将stack2中的节点再转移回stack1
     public int pop() {
         while (!stack1.isEmpty())
             stack2.push(stack1.pop());
--- JianZhiOffer/JianZhiOffer-Java/JianZhiOffer05_Queue_with_Two_Stacks/Solution1.java
+++ JianZhiOffer/JianZhiOffer-Java/JianZhiOffer05_Queue_with_Two_Stacks/Solution1.java
@@ -1,4 +1,4 @@
-package JianZhiOffer05_Queue_with_Two_Stacks;
+package JianZhiOffer.JianZhiOffer_Java.JianZhiOffer05_Queue_with_Two_Stacks;
 
 import java.util.Stack;
 
@@ -6,10 +6,12 @@ public class Solution1 {
     Stack<Integer> stack1 = new Stack<>();
     Stack<Integer> stack2 = new Stack<>();
 
+    //进队节点全部压入stack1
     public void push(int node) {
         stack1.push(node);
     }
 
+    //当stack2不为空时，栈顶为出队节点，当stack2为空时，将stack1中的节点转移至stack2，并将stack2栈顶节点出队
     public int pop() {
         if (stack2.isEmpty()) {
             while (!stack1.isEmpty())
