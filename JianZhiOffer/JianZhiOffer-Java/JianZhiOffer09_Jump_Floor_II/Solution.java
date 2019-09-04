ec2a110bea917dc7d574a5d74f7c2ecb8b73a373
chenshuaiyu <664251867@qq.com>
2019-09-04 19:58:47 +0800

    review JianZhiOffer09

--- JianZhiOffer/JianZhiOffer-Java/JianZhiOffer09_Jump_Floor_II/Solution.java
+++ JianZhiOffer/JianZhiOffer-Java/JianZhiOffer09_Jump_Floor_II/Solution.java
@@ -1,6 +1,11 @@
-package JianZhiOffer09_Jump_Floor_II;
+package JianZhiOffer.JianZhiOffer_Java.JianZhiOffer09_Jump_Floor_II;
 
 public class Solution {
+
+//    f(1) = f(1 - 1) = 1
+//    f(2) = f(2 - 1) + f(2 - 2) = 2
+//    f(3) = f(3 - 1) + f(3 - 2) + f(3 - 3) = 4
+//    ...
     public int JumpFloorII(int target) {
         return 1 << --target;
     }
