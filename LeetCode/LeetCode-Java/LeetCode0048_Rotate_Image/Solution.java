58a4c96a4802ebd8ef64c9d44839c20e0d6192fc
chenshuaiyu <664251867@qq.com>
2020-03-25 17:55:36 +0800

    LeetCode0048

---
+++ LeetCode/LeetCode-Java/LeetCode0048_Rotate_Image/Solution.java
@@ -0,0 +1,18 @@
+package LeetCode.LeetCode_Java.LeetCode0048_Rotate_Image;
+
+public class Solution {
+    public void rotate(int[][] matrix) {
+        if (matrix == null || matrix.length == 0)
+            return;
+        int n = matrix.length;
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = i; j < n - i - 1; j++) {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+}
